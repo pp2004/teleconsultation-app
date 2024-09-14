c7e0f219ee9993f71002f0232d98d76ad31fc9b1
pp2004 <prakash.paarth@gmail.com>
2024-09-15 02:07:24 +0530

    Linked Backend to Frontend

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://Paasuj:Paasuj%402004@kioskcluster0.ho8nv.mongodb.net/?retryWrites=true&w=majority&appName=KioskCluster0', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect('mongodb+srv://Paasuj:Paasuj%402004@kioskcluster0.ho8nv.mongodb.net/?retryWrites=true&w=majority&appName=KioskCluster0');
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
