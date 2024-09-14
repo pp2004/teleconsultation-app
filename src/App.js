e554b37896109f1fea1573c30eb6450777e5f75b
pp2004 <prakash.paarth@gmail.com>
2024-09-14 23:33:43 +0530

    Fix file casing for VideoChat component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
-import React from 'react';
-import VideoChat from './components/VideoChat';
-
-
+import VideoChat from './components/VideoChat.js';
 
 function App() {
   return (
@@ -27,15 +24,12 @@ function App() {
         </a>
       </header>
       
-      <div className="App">
-      <h1>Teleconsultation App</h1>
-      <VideoChat />
-      </div>
       {/* Routing logic */}
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/videochat" element={<VideoChat />} /> {/* Add this route */}
         </Routes>
       </Router>
     </div>
